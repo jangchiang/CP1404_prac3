ae9780f24f5dade412ef353c3b873cc81685add1
Unknown-user-C <42628079+jangchiang@users.noreply.github.com>
2021-11-26 19:52:19 +0800

    password_check

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 """CP1404 Theeradon Somsri practical3"""
 '''password check'''
 
-def mainfunction():
-    print("hi")
+def main():
+    """Start Program."""
+    password = input("Enter a password")
 
-mainfunction()
+    if check_password(password):
+        print("Logged in")
+    else:
+        print("Illegal access")
+
+
+def check_password(password):
+    return password == "12345678"
+
+
+main()
